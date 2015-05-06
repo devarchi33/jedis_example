4ae715bc2697cd5fc285cd2f88337dd39f295569
skyfly33 <imfly7@naver.com>
2015-05-06 11:41:13 +0900

    @redis pop예제 추가.

---
+++ src/main/java/jedis_example/jedis_example/RedisPopJava.java
@@ -0,0 +1,27 @@
+package jedis_example.jedis_example;
+
+import java.util.List;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+
+public class RedisPopJava {
+
+	public static void main(String[] args) {
+		try {
+			// Connecting to Redis server
+			Jedis jedis = new Jedis("192.168.0.20");
+			System.out.println("Connection successfully!!");
+			// get the stored data and print it
+			List<String> list = jedis.lrange("log", 0, -1);
+			for (int i = 0; i < list.size(); i++) {
+				// System.out.println("Stored string in redis[" + (i+1) + "] : "
+				// + list.get(i));
+				String log = jedis.lpop("log");
+				System.out.println("log [" + (i + 1) + "] : " + log);
+			}
+		} catch (JedisConnectionException e) {
+			e.printStackTrace();
+		}
+	}
+}
